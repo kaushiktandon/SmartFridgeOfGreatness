fd9edc9bbd9abfafaa0cb411cb9acb08172dab8d
kaushiktandon <kaushikt@usc.edu>
2018-01-20 20:58:13 -0800

    saves to firebase

--- recipetester.py
+++ recipetester.py
@@ -1,8 +1,17 @@
+# encoding=utf8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
 import json
 import requests
 import urllib
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
-ingredients = ['onions','garlic','milk','eggs','ham','cheese']
+ingredients = [u'rice',u'lettuce',u'milk',u'garlic',u'chicken',u'cheese',u'onion']
 url = 'http://www.recipepuppy.com/api/?'
 
 class Recipe:
@@ -41,57 +50,54 @@ def common_elements(list1, list2):
 def different_elements(list1, list2):
 	return list(set(list2) - set(list1))
 
-def getRecipes(ingredients):
+def generateRecipes(ingredients):
+	cred = credentials.Certificate('/Users/kaush/SmartFridgeOfGreatness/demotesterito-2e621f253c03.json')
+	firebase_admin.initialize_app(cred, {'projectId': 'demotesterito',})
+	db = firestore.client()
+
 	sets = subsets(ingredients) # get different combinations of ingredients
-	recipes = list()
-	pageNumber = 1
 	i = 0
+	invalidCount = 0
 	for theSet in sets:
-		#no point if only one ingredient
-		if len(theSet) < 2:
-			i = i+1
-			continue
-		if i < len(sets)-1:
-			i = i + 1
+		pageNumber = 1
+		#no point if only 2 ingredients
+		if len(theSet) < 3:
 			continue
 		ing = ",".join(theSet)
-		if(pageNumber == 1):#while(pageNumber < 5):
+		while(pageNumber < 6): #60 results
 			tempUrl = url + urllib.urlencode({"i":ing,"p":pageNumber})
-			response = requests.get(tempUrl).json()
-			for j in range(len(response['results'])):
-				name =  response['results'][j]['title']
-				href = response['results'][j]['href']
-				ingr = list(str(response['results'][j]['ingredients']).replace(' ','').split(','))
-				common = common_elements(ingredients,ingr)
-				different = different_elements(ingredients,ingr)
-				print len(common), len(different), name, href
-				recipes.append(Recipe(name,href,ingr,common,different))
+			try:
+				response = requests.get(tempUrl).json()
+				i = i + 1
+				for j in range(len(response['results'])):
+					name =  response['results'][j]['title']
+					href = response['results'][j]['href']
+					ingr = list((response['results'][j]['ingredients']).strip().split(', '))
+					common = common_elements(ingredients,ingr)
+					different = different_elements(ingredients,ingr)
+					if(len(different) == 0): #no missing ingredients
+						#access firestore
+						doc_ref = db.collection(u'Recipes').document(name)
+						doc_ref.set({
+						    u'name': name,
+						    u'url': href,
+						    u'ingredients': ingr,
+						    u'Missing Ingredients': different,
+						    u'Common Ingredients': common
+						})
+			except ValueError, e: #random json encoding error
+				invalidCount = invalidCount + 1
 			pageNumber = pageNumber + 1
-		i = i + 1
-	return recipes
+	#print i # number of api calls
 
-recipes = getRecipes(ingredients)
-
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-
-# Use the application default credentials
-#cred = credentials.ApplicationDefault()
-cred = credentials.Certificate('/Users/kaush/SmartFridgeOfGreatness/demotesterito-2e621f253c03.json')
-
-firebase_admin.initialize_app(cred, {
-  'projectId': 'demotesterito',
-})
+generateRecipes(ingredients)
 
 
-db = firestore.client()
-doc_ref = db.collection(u'Recipes').document(u'alovelace')
 #db.collection(u'Recipes').add(recipes[0].to_dict())
-doc_ref.set({
+#doc_ref.set({
 #    u'name': u'Ada',
 #    u'url': u'espn.go.com',
-#    u'ingredients': [{'a','b'}]
+#    u'ingredients': ingredients,
 #    u'Missing Ingredients': ingredients,
 #    u'Common Ingredients': ingredients
-})
+#})
